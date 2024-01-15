157598d5c534cb7952e36db8c04087de2de1751d
praneeth@g <gpraneeth22@gmail.com>
2024-01-15 23:02:48 +0530

    Added App Envs

---
+++ src/lib/AppEnvironment.ts
@@ -0,0 +1,20 @@
+interface EnvVariables {
+    apiUrl: string;
+  }
+  
+  interface Environments {
+    development: EnvVariables;
+    production: EnvVariables;
+  }
+  
+  const appConfig: Environments = {
+    development: {
+        apiUrl: 'http://localhost:3000/api/trpc',
+    },
+    production: {
+        apiUrl: 'https://legal-intellect.gpdev.me'
+    },
+  };
+
+export const config: EnvVariables = appConfig["production"];
+  
