282baa6ef8fc4df9141d8ff5ea64d5f8986b7ec4
praneeth@g <gpraneeth22@gmail.com>
2024-01-16 20:42:57 +0530

    PDF Rendering Completed. Starting Chat Component

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,53 +1,53 @@
-import ChatBox from "@/components/ChatBox";
-import PdfRenderer from "@/components/PdfRenderer";
-import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { notFound, redirect } from "next/navigation";
-import { use } from "react";
+import ChatBox from '@/components/ChatBox'
+import PdfRenderer from '@/components/PdfRenderer'
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { notFound, redirect } from 'next/navigation'
 
 interface PageProps {
   params: {
-    fileId: string;
-  };
+    fileid: string
+  }
 }
 
-const DocumentPage = async ({ params }: PageProps) => {
-  // fetch file id
-  const { fileId } = params;
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params
 
-  if (!user || !user.id) redirect(`/auth-callback?`);
-  // make db call
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+  if (!user || !user.id)
+    redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
   const file = await db.file.findFirst({
     where: {
-      id: fileId,
+      id: fileid,
       userId: user.id,
     },
-  });
+  })
 
-  //   if (!file) notFound();
+  if (!file) notFound()
+
+  // const plan = await getUserSubscriptionPlan()
 
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
-      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+    <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+      <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         {/* Left sidebar & main wrapper */}
-        <div className="flex-1 xl:flex">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
             {/* Main area */}
-            {/* <PdfRendererrer url={file.url} /> */}
-            <PdfRenderer />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
-        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          {/* <ChatBox isSubscribed={plan.isSubscribed} fileId={file.id} /> */}
-          <ChatBox />
+        <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
+          {/* <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} /> */}
+          <ChatBox/>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DocumentPage;
+export default Page
